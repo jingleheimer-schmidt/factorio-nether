6b76aecedb68d17a9c7acfe64f97e81fa3e24a5b
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-16 22:56:00 -0700

    updated collision and selection boxes

--- nether-portal.lua
+++ nether-portal.lua
@@ -8,7 +8,9 @@ local netherPortal = {
     width = 150,
     height = 150
   },
-  collision_box = {{-2, -2}, {2, 2}}
+  collision_box = {{-2, -2}, {2, 2}},
+  collision_mask = {"item-layer", "object-layer", "water-tile"},
+  selection_box = {{-2, -2}, {2, 2}}
 }
 
 data:extend({
