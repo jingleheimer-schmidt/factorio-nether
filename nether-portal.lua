932a778383223bd74cbddc76e24eae52385b6913
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-12-18 08:04:49 -0800

    re-added nether portal simple entity

--- nether-portal.lua
+++ nether-portal.lua
@@ -1,131 +1,131 @@
--- local netherPortal = {
---   type = "simple-entity",
---   name = "nether-portal",
---   icon = "__factorio-nether__/graphics/Nether_portal_squish.png",
---   icon_size = 250,
---   animations = {
---     sheets = {
---       {
---         filename = "__factorio-nether__/graphics/portal_frame_variations_block.png",
---         width = 64,
---         height = 80,
---         variation_count = 31,
---         frame_count = 31,
---         scale = 2,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {-.5, -1}, --top left
---         scale = 1,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {.5, -1}, --top right
---         scale = 1,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {-.5, 0}, --middle left
---         scale = 1,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {.5, 0}, --middle right
---         scale = 1,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {-.5, 1}, --bottom left
---         scale = 1,
---         animation_speed = .3
---       },
---       {
---         filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
---         width = 32,
---         height = 32,
---         variation_count = 31,
---         frame_count = 31,
---         shift = {.5, 1}, --bottom right
---         scale = 1,
---         animation_speed = .3
---       }
---     }
---   },
---   integration_patch = {
---     sheet = {
---       filename = "__factorio-nether__/graphics/integration_patch_512_v2.png",
---       width = 512,
---       height = 512,
---       frames = 1,
---       scale = .5
---     }
---   },
---   integration_patch_render_layer = "ground-patch-higher2",
---   random_variation_on_create = "true",
---   collision_box = {{-2, -2.5}, {2, 2.5}},
---   collision_mask = {"item-layer", "object-layer", "water-tile"},
---   selection_box = {{-2, -2.5}, {2, 2.5}},
---   render_layer = "floor",
---   map_color = {r=142, g=70, b=218}, -- don't define alpha
---   remove_decoratives = "true",
---   allow_copy_paste = "false",
---   -- trigger_radius = 2,
---   -- timeout = 230,
---   -- action =
---   -- {
---   --   type = "direct",
---   --   action_delivery =
---   --   {
---   --     type = "instant",
---   --     target_effects =
---   --     {
---   --       {
---   --         type = "script",
---   --         effect_id = "into-portal"
---   --       }
---   --     }
---   --   }
---   -- },
---   -- force_die_on_attack = false,
---   -- trigger_force = "all",
---   minable = {
---     mining_time = 32,
---     result = "nether-portal-item"
---   },
---   flage = {
---     "placeable-neutral",
---     "placeable-player",
---     "player-creation",
---     "not-upgradable",
---     "no-copy-paste",
---     "not-blueprintable"
---   }
--- }
+local netherPortal = {
+  type = "simple-entity",
+  name = "nether-portal",
+  icon = "__factorio-nether__/graphics/Nether_portal_squish.png",
+  icon_size = 250,
+  animations = {
+    sheets = {
+      {
+        filename = "__factorio-nether__/graphics/portal_frame_variations_block.png",
+        width = 64,
+        height = 80,
+        variation_count = 31,
+        frame_count = 31,
+        scale = 2,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {-.5, -1}, --top left
+        scale = 1,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {.5, -1}, --top right
+        scale = 1,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {-.5, 0}, --middle left
+        scale = 1,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {.5, 0}, --middle right
+        scale = 1,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {-.5, 1}, --bottom left
+        scale = 1,
+        animation_speed = .3
+      },
+      {
+        filename = "__factorio-nether__/graphics/portal_animation_block_32px.png",
+        width = 32,
+        height = 32,
+        variation_count = 31,
+        frame_count = 31,
+        shift = {.5, 1}, --bottom right
+        scale = 1,
+        animation_speed = .3
+      }
+    }
+  },
+  integration_patch = {
+    sheet = {
+      filename = "__factorio-nether__/graphics/integration_patch_512_v2.png",
+      width = 512,
+      height = 512,
+      frames = 1,
+      scale = .5
+    }
+  },
+  integration_patch_render_layer = "ground-patch-higher2",
+  random_variation_on_create = "true",
+  collision_box = {{-2, -2.5}, {2, 2.5}},
+  collision_mask = {"item-layer", "object-layer", "water-tile"},
+  selection_box = {{-2, -2.5}, {2, 2.5}},
+  render_layer = "floor",
+  map_color = {r=142, g=70, b=218}, -- don't define alpha
+  remove_decoratives = "true",
+  allow_copy_paste = "false",
+  -- trigger_radius = 2,
+  -- timeout = 230,
+  -- action =
+  -- {
+  --   type = "direct",
+  --   action_delivery =
+  --   {
+  --     type = "instant",
+  --     target_effects =
+  --     {
+  --       {
+  --         type = "script",
+  --         effect_id = "into-portal"
+  --       }
+  --     }
+  --   }
+  -- },
+  -- force_die_on_attack = false,
+  -- trigger_force = "all",
+  minable = {
+    mining_time = 1,
+    result = "nether-portal-item"
+  },
+  flags = {
+    "placeable-neutral",
+    "placeable-player",
+    "player-creation",
+    "not-upgradable",
+    "no-copy-paste",
+    "not-blueprintable"
+  }
+}
 
 local netherPortalItem = {
   type = "item",
@@ -133,7 +133,7 @@ local netherPortalItem = {
   icon = "__factorio-nether__/graphics/Nether_portal_squish.png",
   icon_size = 250,
   stack_size = 1,
-  place_result = "nether-portal-landmine"
+  place_result = "nether-portal"
 }
 
 local netherPortalLandmine = {
@@ -187,7 +187,7 @@ local netherPortalLandmine = {
   },
   trigger_radius = 2,
   trigger_force = "all",
-  timeout = 230,
+  timeout = 10,
   ammo_category = "landmine",
   force_die_on_attack = "false",
   action =
@@ -241,12 +241,12 @@ local netherPortalLandmineSticker = {
   --icon = "__base__/graphics/icons/slowdown-sticker.png",
   flags = {},
   animation = util.empty_sprite(),
-  duration_in_ticks = 1,
+  duration_in_ticks = 210,
   --target_movement_modifier = 1
 }
 
 data:extend({
---   netherPortal,
+  netherPortal,
   netherPortalItem,
   netherPortalLandmine,
   netherPortalLandmineSticker
