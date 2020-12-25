3e42ce763cbfd5c536268b78c0be24fda5651155
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-12-24 16:21:32 -0800

    forgot ()

--- control.lua
+++ control.lua
@@ -23,7 +23,7 @@ end)
 script.on_event(defines.events.on_trigger_created_entity, function(event)
 -- MMAKE SURE CREATED ENTITY WAS THE PORTAL STICKER
   game.print(game.tick .. "landmine created entity")
-  if not event.entity.type == "character" then
+  if not (event.entity.type == "character") then
     game.print("entity is not character")
     return
   end
