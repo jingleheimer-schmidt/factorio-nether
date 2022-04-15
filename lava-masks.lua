afed86d613c23b43b91809abd5208ae41d0b5da2
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-04-14 22:07:11 -0700

    Fixed that some entities could walk on water when they shouldn't

--- lava-masks.lua
+++ lava-masks.lua
@@ -4,43 +4,32 @@ local layer = collision_mask_util.get_first_unused_layer()
 
 for a,b in pairs(data.raw.tile["lava"].collision_mask) do
   if b == "player-layer" then
-    table.remove(data.raw.tile["lava"].collision_mask, a)
-    collision_mask_util.add_layer(data.raw.tile["lava"].collision_mask, layer)
+    collision_mask_util.remove_layer(data.raw.tile["lava"].collision_mask, b)
+    -- table.remove(data.raw.tile["lava"].collision_mask, a)
   end
+  collision_mask_util.add_layer(data.raw.tile["lava"].collision_mask, layer)
 end
 
 for a,b in pairs(data.raw["unit"]) do
-  if b.collision_mask then
-    collision_mask_util.add_layer(data.raw["unit"][a].collision_mask, layer)
-  else
-    data.raw["unit"][a].collision_mask = {}
-    collision_mask_util.add_layer(data.raw["unit"][a].collision_mask, layer)
-  end
+  local mask = collision_mask_util.get_mask(b)
+  collision_mask_util.add_layer(mask, layer)
+  b.collision_mask = mask
 end
 
 for a,b in pairs(data.raw["car"]) do
-  if b.collision_mask then
-    collision_mask_util.add_layer(data.raw["car"][a].collision_mask, layer)
-  else
-    data.raw["car"][a].collision_mask = {}
-    collision_mask_util.add_layer(data.raw["car"][a].collision_mask, layer)
-  end
+  local mask = collision_mask_util.get_mask(b)
+  collision_mask_util.add_layer(mask, layer)
+  b.collision_mask = mask
 end
 
 for a,b in pairs(data.raw["spider-vehicle"]) do
-  if b.collision_mask then
-    collision_mask_util.add_layer(data.raw["spider-vehicle"][a].collision_mask, layer)
-  else
-    data.raw["spider-vehicle"][a].collision_mask = {}
-    collision_mask_util.add_layer(data.raw["spider-vehicle"][a].collision_mask, layer)
-  end
+  local mask = collision_mask_util.get_mask(b)
+  collision_mask_util.add_layer(mask, layer)
+  b.collision_mask = mask
 end
 
 for a,b in pairs(data.raw["spider-leg"]) do
-  if b.collision_mask then
-    collision_mask_util.add_layer(data.raw["spider-leg"][a].collision_mask, layer)
-  else
-    data.raw["spider-leg"][a].collision_mask = {}
-    collision_mask_util.add_layer(data.raw["spider-leg"][a].collision_mask, layer)
-  end
+  local mask = collision_mask_util.get_mask(b)
+  collision_mask_util.add_layer(mask, layer)
+  b.collision_mask = mask
 end
